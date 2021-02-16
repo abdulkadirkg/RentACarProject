9341607a13534193a1da79348f87427d4f0c5376
AbdulkadirKG <sipahiyazilim@gmail.com>
2021-02-16 18:39:16 +0300

    Rent A Car Project

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -40,6 +40,7 @@ namespace ConsoleUI
                 Description = "Audi A7 250 HP",
                 ModelYear = 2020
             };
+            //
             Console.WriteLine("---ADD---------------------------------------");
             carManager.Add(car);
             carManager.Add(car2);
